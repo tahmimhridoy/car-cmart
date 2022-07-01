526f4283bf079f4d70058c38992dc2e232c5fed2
tahmimhridoy <thamimhasanhridoyea@gmail.com>
2022-07-01 20:40:30 +0600

    touch section

---
+++ src/components/pages/GetInTouch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GetInTouch.css';
+
+const GetInTouch = () => {
+    return (
+        <div className='getTouch'>
+            <div className="touch mx-20">
+                <h1>Do You Have Questions ?</h1>
+                <button>GET IN TOUCH</button>
+            </div>
+        </div>
+    );
+};
+
+export default GetInTouch;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,7 @@ import Agent from './Agent';
 import Banner from './Banner';
 import CarBlog from './CarBlog';
 import Featured from './Featured';
+import GetInTouch from './GetInTouch';
 import Offers from './Offers';
 import Search from './Search';
 import Testimonial from './Testimonial';
@@ -19,6 +20,7 @@ const Home = () => {
             <Agent />
             <Testimonial />
             <CarBlog />
+            <GetInTouch />
         </div>
     );
 };
